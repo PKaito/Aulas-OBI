4a10d5beae77288c1f6478c582b9ee472cfdec6a
PKaito <142739096+PKaito@users.noreply.github.com>
2026-04-18 13:46:47 +0000

    Ponteiros

--- AULAS OBI/Problemas da OBI/Kilauea.cpp
+++ AULAS OBI/Problemas da OBI/Kilauea.cpp
@@ -1,12 +1,19 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int void(main){
 int N, F;
 cin >> N >> F;
-vector 
+
+char matrix[N][N];
+
+for (int i = 0; i < N; i++){
+    for (int j = 0; j < N; j++){
+         cin >> matrix[i][j];
+    }
+}
+
 
 
 
---
+++ AULAS OBI/Problemas da OBI/ponteiros.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int i = 10;
+
+    int* p;
+
+    p = &i;
+
+    cout << i << " " << *p << "\n";
+return 0;
+}
